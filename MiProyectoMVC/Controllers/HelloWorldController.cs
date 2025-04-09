6da287a53111944dc5aa50be2ac9c4d9cfd6b182
EvelynEnc <evelyn2001mtd@gmail.com>
2025-04-09 16:35:18 -0300

    Agregado Welcome.cshtml y actualización del controlador

--- MiProyectoMVC/Controllers/HelloWorldController.cs
+++ MiProyectoMVC/Controllers/HelloWorldController.cs
@@ -3,20 +3,27 @@ using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Mvc;
 using MiProyectoMVC.Models;
 
-namespace MiProyectoMVC.Controllers
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+
+namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // GET: /HelloWorld/
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
-        // GET: /HelloWorld/Welcome/
-        public string Welcome(string name, int numTimes = 1)
+
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+
+            return View();
         }
     }
 }
